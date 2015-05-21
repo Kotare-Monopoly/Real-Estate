66370274b2a2094103fdc5da5ad4bbd7b77e85a8
Rich Churcher <rich.churcher@gmail.com>
2015-05-21 18:11:31 +1200

    The mystery of the missing migration

---
+++ RealEstate/Migrations/201505210558234_init.cs
@@ -0,0 +1,26 @@
+namespace RealEstate.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class init : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "realestate.Properties",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Hours = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("realestate.Properties");
+        }
+    }
+}
